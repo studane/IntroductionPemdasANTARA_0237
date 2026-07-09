c8c2aeaa15c6409b536b38c5ec4cd5d8c69ff8ad
studane <zhidaneprabowoguzlend@gmail.com>
2026-07-09 15:26:56 +0700

    membuat prosedur

--- pertemuan2b.cpp
+++ pertemuan2b.cpp
@@ -3,3 +3,14 @@ using namespace std;
 
 //Numeric nPanjang, nLebar, nKeliling
 int Panjang, Lebar, Keliling;
+
+void Input(){
+    //Display "Masukkan Panjang : "
+    cout << "Masukkan Panjang : ";
+    //Accept nPanjang
+    cin >> Panjang;
+    //Display "Masukkan Lebar : "
+    cout << "Masukkan Lebar : ";
+    //Accept nLebar
+    cin >> Lebar;
+}
