94242e44b4b93a37d380e7f7b58603c53a071340
studane <zhidaneprabowoguzlend@gmail.com>
2026-07-09 15:08:59 +0700

    finish

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int Panjang;
+    int Lebar; 
+    string nama = "Abdul";
+    float harga = 50000;
+    double harga2 = 1000000;
+
+    cout << "Nama saya : " << nama << endl;
+    cout << "Harga makanan itu : " << harga;
+}
